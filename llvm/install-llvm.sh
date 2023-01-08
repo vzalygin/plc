c5a44643855714f394c2174c5e7cc66b5afd5a10
Slava Zalygin <vya.zalygin@gmail.com>
2023-01-08 22:13:32 +0300

    add fix

--- llvm/install-llvm.sh
+++ llvm/install-llvm.sh
@@ -2,3 +2,4 @@
 wget https://apt.llvm.org/llvm.sh
 chmod +x llvm.sh
 sudo ./llvm.sh 10 all
+sudo apt-get install zlib1g-dev # fixes linking errs: https://stackoverflow.com/questions/3373995/usr-bin-ld-cannot-find-lz

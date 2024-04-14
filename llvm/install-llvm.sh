e34bbd19c91e08b2794e9a4474c0c14ef84cb6e6
Vyacheslav Zalygin <vya.zalygin@gmail.com>
2024-04-14 18:45:43 +0000

    remove everythink

--- llvm/install-llvm.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-wget https://apt.llvm.org/llvm.sh
-chmod +x llvm.sh
-sudo ./llvm.sh 12 all
-sudo apt-get install zlib1g-dev # fixes linking errs: https://stackoverflow.com/questions/3373995/usr-bin-ld-cannot-find-lz

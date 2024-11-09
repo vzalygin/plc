24e715c511e5039fb86162fcfbd53a84e146f512
Vyacheslav Zalygin <vya.zalygin@gmail.com>
2024-11-09 17:25:32 +0300

    feat[e2e]: add e2e tests project, compiler api, some simple tests

--- e2e/src/lib.rs
+++ e2e/src/lib.rs
@@ -1,24 +1,48 @@
 #[allow(dead_code)]
-
 mod util;
 
 #[cfg(test)]
 mod tests {
-    use anyhow::Result;
-
     use crate::util::Compiler;
 
+    use anyhow::Result;
+    use lazy_static::lazy_static;
+
+    lazy_static! {
+        static ref compiler: Compiler = Compiler::make()
+            .unwrap_or_else(|e| { panic!("Failed to build a compiler with: {}", e) });
+    }
+
     #[test]
-    fn successful_build() -> Result<()> {
-        Compiler::make().map(|_| { () })
+    fn print_command() -> Result<()> {
+        let input = "1 .";
+        let exp = "1\n";
+
+        let act = &compiler.compile(input)?.and_execute_once()?;
+
+        assert_eq!(exp, act);
+        Ok(())
     }
 
     #[test]
     fn add_command() -> Result<()> {
-        let compiler = Compiler::make()?;
+        let input = "1 2 + .";
+        let exp = "3\n";
 
-        compiler.compile("123");
+        let act = &compiler.compile(input)?.and_execute_once()?;
 
+        assert_eq!(exp, act);
+        Ok(())
+    }
+
+    #[test]
+    fn help_message() -> Result<()> {
+        let input = ["-h"];
+        let exp = "postfix language compiler\n\nUsage: plc [OPTIONS] <FILE>\n\nArguments:\n  <FILE>  \n\nOptions:\n  -S, --compile-only   Only compile file to nasm; do not assemble or link\n  -c, --assemble-only  Compile and assemble, but do not link\n  -o, --output <FILE>  Place the output file into FILE\n  -h, --help           Print help\n  -V, --version        Print version\n";
+
+        let act = compiler.run_command(input)?;
+
+        assert_eq!(act, exp);
         Ok(())
     }
 }
--- e2e/src/util.rs
+++ e2e/src/util.rs
@@ -1,12 +1,36 @@
-use core::str;
-use std::{env, ffi::OsStr, path::PathBuf, process::{Command, ExitStatus, Stdio}};
 use anyhow::{anyhow, Result};
+use core::str;
+use std::{
+    env,
+    ffi::OsStr,
+    path::PathBuf,
+    process::{Command, ExitStatus, Output, Stdio},
+};
 
 const TMP_SUBDIR: &str = "plc_e2e";
 
-pub struct Output {
-    status: ExitStatus,
-    output: String,
+pub struct CompilationResult {
+    pub compilation_output: Output,
+    pub output_file: Option<PathBuf>,
+}
+
+impl CompilationResult {
+    pub fn new(compilation_output: Output, output_file: Option<PathBuf>) -> CompilationResult {
+        CompilationResult {
+            compilation_output,
+            output_file,
+        }
+    }
+
+    pub fn and_execute_once(self) -> Result<String> {
+        if let Some(file) = &self.output_file {
+            let result = run_command(&mut Command::new(file));
+            let _ = std::fs::remove_file(file);
+            result
+        } else {
+            Err(anyhow!("no output file"))
+        }
+    }
 }
 
 #[derive(Clone, Debug)]
@@ -24,70 +48,78 @@ impl Compiler {
             .args(["build", "--verbose", "--manifest-path", "../app/Cargo.toml"])
             .stdout(Stdio::null())
             .stderr(Stdio::null())
-            .status()?;
+            .output()?;
 
         let pwd = Command::new("pwd").output()?;
-    
-        if !build.success() {
-            return Err(anyhow!("Build returned non-zero exit code: {}", map_err_status(build)))
+
+        if !build.status.success() {
+            return Err(anyhow!(
+                "Build returned non-zero exit code: {}",
+                map_err_status(build.status)
+            ));
         }
 
         if !pwd.status.success() {
-            return Err(anyhow!("Pwd command returned non-zero exit code: {}", map_err_status(pwd.status)))
+            return Err(anyhow!(
+                "Pwd command returned non-zero exit code: {}",
+                map_err_status(pwd.status)
+            ));
         }
 
         check_tmp_dir()?;
 
-        let executable = String::from_utf8(pwd.stdout)? + "/../target/debug/plc"; 
-        Ok(Compiler::new(PathBuf::from(executable)))
+        let executable_path = String::from_utf8(pwd.stdout.split_last().unwrap().1.to_vec())?
+            + "/../target/debug/plc";
+        Ok(Compiler::new(PathBuf::from(executable_path)))
     }
 
     pub fn run_command<A, S>(&self, args: A) -> Result<String>
     where
-    A: IntoIterator<Item = S>,
-    S: AsRef<OsStr> {
-        run_command(
-            &mut Command::new(self.executable.as_path())
-                .args(args)
-        )
+        A: IntoIterator<Item = S>,
+        S: AsRef<OsStr>,
+    {
+        run_command(Command::new(self.executable.as_path()).args(args))
     }
 
-    pub fn compile(&self, input: &str) -> Result<String> {
-        self.translate(input, [""], true)
+    pub fn compile(&self, input: &str) -> Result<CompilationResult> {
+        self.compile_with_args(input, &[])
     }
 
-    pub fn translate<A, S>(&self, input: &str, args: A, execute: bool) -> Result<String>
-    where
-    A: IntoIterator<Item = S>,
-    S: AsRef<OsStr> {
+    pub fn compile_with_args(&self, input: &str, args: &[&str]) -> Result<CompilationResult> {
         let input_path = make_tmp_path();
         let output_path = make_tmp_path();
 
         std::fs::write(&input_path, input)?;
-        
-        let run_result = run_command(
-            &mut Command::new(self.executable.as_path())
-                .args(args)
-                .args(["--output"]).args([&output_path, &input_path])
-        );
 
-        let output = if execute {
-
-        } else {
-            std::fs::read_to_string(&output_path)?;
-        };
+        let compilation_output = Command::new(self.executable.as_path())
+            .args(args)
+            .args(["--output"])
+            .args([&output_path, &input_path])
+            .output();
 
         let _ = std::fs::remove_file(&input_path);
-        let _ = std::fs::remove_file(&output_path);
 
-        run_result?;
+        let compilation_output = compilation_output?;
+        if !compilation_output.status.success() {
+            return Err(anyhow!(
+                "{}",
+                str::from_utf8(compilation_output.stderr.as_slice())?
+            ));
+        }
 
-        Ok(output)
+        let output_file_path = if output_path.exists() {
+            Some(output_path)
+        } else {
+            None
+        };
+
+        Ok(CompilationResult::new(compilation_output, output_file_path))
     }
 }
 
 fn map_err_status(status: ExitStatus) -> String {
-    status.code()
+    status
+        .code()
         .map(|x| x.to_string())
         .unwrap_or("no-code".to_string())
 }
@@ -99,14 +131,11 @@ fn make_tmp_path() -> PathBuf {
 }
 
 fn check_tmp_dir() -> Result<()> {
-    std::fs::create_dir_all(env::temp_dir().join(TMP_SUBDIR))
-        .map_err(|e| e.into())
+    std::fs::create_dir_all(env::temp_dir().join(TMP_SUBDIR)).map_err(|e| e.into())
 }
 
 fn run_command(prepared_command: &mut Command) -> Result<String> {
-    let output = prepared_command
-        .arg("2>&1")
-        .output()?;
+    let output = prepared_command.arg("2>&1").output()?;
 
     if output.status.success() {
         Ok(String::from_utf8(output.stdout)?)
